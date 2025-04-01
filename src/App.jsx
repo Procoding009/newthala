14ce44fe4afdfb207adf752e5b4acc6b9db632d8
b <b>
2025-04-01 10:04:28 +0530

    NEW

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,17 @@ const App = () => {
   return (
     <>
       <div className="bg-[#0f1b31] min-h-screen flex flex-col items-center text-white px-4 py-10 md:py-20">
-        {/* Heading Section */}
+        
         <p className="text-md sm:text-lg mb-2 text-center">
           || INDIA'S MOST TRUSTED <br /> & OLDEST BOOK ||
         </p>
         <h1 className="text-3xl sm:text-4xl font-bold text-yellow-400 p-4 md:p-10 text-center">
-          ThalaPathybook
+            THALAPATHYONLINEBOOK
         </h1>
         <p className='p-2 sm:p-3 text-center'>GET YOUR !D NOW ON WHATSAPP👇🏼</p>
         <p className="text-lg mb-4 animate-updown">👇👇</p>
 
-        {/* WhatsApp Button */}
+
         <a
           href={whatsappUrl}
           target="_blank"
@@ -31,14 +31,14 @@ const App = () => {
           <FaWhatsapp className="text-2xl" />
         </a>
 
-        {/* Image Section */}
+        
         <div className="p-5 sm:p-10 w-full max-w-xs sm:max-w-md">
           <a href={whatsappUrl} target="_self">
             <img src="/images/h1.png" alt="image" className="w-full rounded-lg shadow-lg" />
           </a>
         </div>
 
-        {/* Info Section */}
+        
         <div className="flex flex-col justify-between text-center space-y-2 sm:space-y-4">
           <p className="p-2">GET 10% BONUS ON FIRST DEPOSIT</p>
           <p className="p-2">24/7 WITHDRAWAL & DEPOSIT</p>
