2700afe8c2b52aa399a9393fd711166172fb02cb
Bharat Dangi <141334107+Procoding009@users.noreply.github.com>
2025-05-08 15:58:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 
 
 const App = () => {
-  const phoneNumber = '+918126579278'
+  const phoneNumber = '+12097649470'
   const message = encodeURIComponent('Hello👋 i want Thalapathy B00k 🆔')
   const whatsappUrl = `https://api.whatsapp.com/send/?phone=${phoneNumber}&text=${message}`
 
